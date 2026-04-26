dc70c449a53678b6e70cb4ac4b32b2882d061033
Qian HE <ho.ykwann@gmail.com>
2026-04-26 16:20:20 +0200

    Refactor PEMFC model initialization and enhance dxdt performance with cached lookups

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ from scipy.integrate import solve_ivp
 from modules.display import display
 
 if __name__ == "__main__":
-    model = PEMFC(param=parameters, variable_names=solver_variable_names, operating_inputs=operating_inputs)
+    model = PEMFC(param=parameters, operating_inputs=operating_inputs,
+                  variable_names=solver_variable_names, flux_names=solver_flux_names)
     solution_init = init_x(operating_inputs, parameters)
-    solution = solve_ivp(fun=model.dxdt, t_span=(0, 10), y0=solution_init, method='BDF')
+    jac_sparsity = model.compute_jac_sparsity(solution_init)
+    solution = solve_ivp(fun=model.dxdt, t_span=(0, 100), y0=solution_init,
+                         method='BDF', jac_sparsity=jac_sparsity)
     print("Simulation done")
     display(solution, model)
     print("Display done")
