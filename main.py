778d07259375f163594da40802ea1becb53f31d5
Qian HE <ho.ykwann@gmail.com>
2026-04-30 17:01:43 +0200

    improve model

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from config.initialize import *
 from config.settings import *
-from model.model import PEMFC
+from model.dualscale import PEMFC
 from scipy.integrate import solve_ivp
 from modules.display import display
 
